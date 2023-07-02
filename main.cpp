792689bceeac436b5e75e8fda64094fb66e05b90
saeed <saeed_99_per@outlook.com>
2023-07-02 20:54:41 +0300

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 // Time Library.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // PROGRAMMER : SAEED LAHMDI
+#pragma warning(disable : 4996)
 
 #include <iostream>
 #include "clsTime.h"
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
+    cout << "Local Time:";
+
+    // get and print local time
+    clsTime::getLocalTime().Print();
+
+    cout << "UTC Time: ";
+    
+    // get and print UTC time
+    clsTime::getUTCtime().Print();
+
     return 0;
 }
