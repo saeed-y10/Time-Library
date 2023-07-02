292b5547ebdb704d7a4efb7986092e4752aaeb11
saeed <saeed_99_per@outlook.com>
2023-07-02 19:35:27 +0300

    add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Time Library.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// PROGRAMMER : SAEED LAHMDI
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
