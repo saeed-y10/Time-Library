709e6691c2cc23598dfc988d6a60400f918c3afd
saeed <saeed_99_per@outlook.com>
2023-07-03 16:02:34 +0300

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,11 +11,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Local Time: ";
-    clsTime::getLocalTime().Print(); // get and print local time
+    clsTime Time1(1, 5, 1), Time2(1, 5, 1);
 
-    cout << "UTC Time: ";
-    clsTime::getUTCtime().Print(); // get and print UTC time
+    cout << "Time 1: ";
+    Time1.Print();
+    
+    cout << "Time 2: ";
+    Time2.Print();
+
+    cout << "Is Time1 Before Time2: " << Time1.IsTimeBeforeTime2(Time2) << endl;
+    cout << "Is Time1 After Time2: " << Time1.IsTimeAfterTime2(Time2) << endl;
+    cout << "Is Time1 Equal Time2: " << Time1.IsTimeEqualTime2(Time2) << endl;
 
     return 0;
 }
